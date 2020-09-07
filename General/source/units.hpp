c24644dc30e01b395c951d62437f5d18373fc9f9
yakun <ihatecapslook@gmail.com>
2020-09-08 01:10:31 +0300

    A

--- General/source/units.hpp
+++ General/source/units.hpp
@@ -2,27 +2,56 @@
 #define HEROES_OF_EANDOM_UNITS_HPP
 
 #include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <utility>
 
 class Unit {
  public:
     Unit(int64_t hp, int64_t min_damage, int64_t max_damage,int64_t armor, int64_t speed,
-        int64_t count=0)
-        :
+        std::string name,
+        int64_t count=0):
         count(count), top_hp(hp),
-        hp(hp), min_damage(min_damage), max_damage(max_damage), armor(armor), speed(speed) {
+        hp(hp), min_damage(min_damage), max_damage(max_damage), armor(armor), speed(speed),
+        name(std::move(name)) {
     }
 
     void Attack(Unit* target) {
         target->armor;
         int64_t total_dmg = 0;
         for (int i = 0; i < count; ++i) {
-            total_dmg += max(random() - target->armor, 0)
+            total_dmg += std::max<int64_t >(random() - target->armor, 0);
         }
+        target->TakeDamage(total_dmg);
     }
 
 
-    void TakeDamage(size_t dmg);
-    bool IsAlive();
+    void TakeDamage(int64_t dmg) {
+        // First
+        if (top_hp < hp) {
+            if (dmg < top_hp) {
+                top_hp -= dmg;
+            }
+            else {
+                dmg -= top_hp;
+                top_hp = hp;
+                count--;
+            }
+        }
+
+        // Mid
+        int64_t killed = dmg / hp;
+        count -= killed;
+        count = std::max<int64_t>(count, 0);
+        dmg -= killed*hp;
+
+        // Last
+        top_hp -= dmg;
+    }
+
+    bool IsAlive() {
+        return count > 0;
+    }
 
     int64_t count;
     int64_t top_hp;
@@ -33,7 +62,7 @@ class Unit {
     const int64_t armor;
     const int64_t speed;
 
-    const std::string
+    const std::string name;
 };
 
 #endif //HEROES_OF_EANDOM_UNITS_HPP
